7043071260628ae276b9b0ea8b9bab12191ecdbf
Guillaume-Larghero <guillaume.larghero@yahoo.com>
2024-03-13 15:20:44 -0400

    update pool

--- Pooling.py
+++ Pooling.py
@@ -2,6 +2,9 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import pandas as pd
+import torch
+
+
 
 class PoolingLayer() :
     '''
@@ -16,9 +19,17 @@ class PoolingLayer() :
         
     def forward(self, x):
         if self.type_of_pool == 'Avg':
-            return torch.mean(x, dim=0)  # Average pooling
+            return torch.mean(x, dim=0)  
         elif self.type_of_pool == 'Max':
-            return torch.max(x, dim=0)[0]  # Max pooling
+            return torch.max(x, dim=0)[0]
         else:
             raise ValueError("Invalid pooling type. Choose 'Avg' or 'Max'.")
         
+        
+if __name__ == '__main__':
+    
+    
+    dummytensor = torch.randn(3,5)
+    print(dummytensor)
+    print(torch.mean(dummytensor, dim =0))
+    print(torch.max(dummytensor, dim=0)[0])
